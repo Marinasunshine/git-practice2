7c41114772f382c87c36478ae47cb0d8480b3bff
MarinaZabotkina <zabotkina06@mail.ru>
2024-12-19 14:34:26 +0300

    Update file_with_error.py

--- file_with_error.py
+++ file_with_error.py
@@ -4,4 +4,4 @@ def create_task(title, description):
     a = random.randint(1, 10)
     b = random.randint(1, 10)
     equation = f"{a}x + {b} = {a + b}"
-    print(f"Создана новая задача: {equation}")
+    print(f"Исправлена критическая ошибка")
